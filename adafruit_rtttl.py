0f5df77cbb956abbc6b0497bd9701f8570f04d01
dherrada <dylan.herrada@gmail.com>
2020-03-15 16:19:15 -0400

    Ran black, updated to pylint 2.x

--- adafruit_rtttl.py
+++ adafruit_rtttl.py
@@ -39,6 +39,7 @@ AUDIOIO_AVAILABLE = False
 try:
     import audioio
     from adafruit_waveform import sine
+
     AUDIOIO_AVAILABLE = True
     try:
         import audiocore
@@ -47,44 +48,47 @@ try:
 except ImportError:
     pass
 
-PIANO = {"4c" : 261.626,
-         "4c#": 277.183,
-         "4d" : 293.665,
-         "4d#": 311.127,
-         "4e" : 329.628,
-         "4f" : 349.228,
-         "4f#": 369.994,
-         "4g" : 391.995,
-         "4g#": 415.305,
-         "4a" : 440,
-         "4a#": 466.164,
-         "4b" : 493.883,
-         "5c" : 523.251,
-         "5c#": 554.365,
-         "5d" : 587.330,
-         "5d#": 622.254,
-         "5e" : 659.255,
-         "5f" : 698.456,
-         "5f#": 739.989,
-         "5g" : 783.991,
-         "5g#": 830.609,
-         "5a" : 880,
-         "5a#": 932.328,
-         "5b" : 987.767,
-         "6c" : 1046.50,
-         "6c#": 1108.73,
-         "6d" : 1174.66,
-         "6d#": 1244.51,
-         "6e" : 1318.51,
-         "6f" : 1396.91,
-         "6f#": 1479.98,
-         "6g" : 1567.98,
-         "6g#": 1661.22,
-         "6a" : 1760,
-         "6a#": 1864.66,
-         "6b" : 1975.53,
-         "7c" : 2093,
-         "7c#": 2217.46}
+PIANO = {
+    "4c": 261.626,
+    "4c#": 277.183,
+    "4d": 293.665,
+    "4d#": 311.127,
+    "4e": 329.628,
+    "4f": 349.228,
+    "4f#": 369.994,
+    "4g": 391.995,
+    "4g#": 415.305,
+    "4a": 440,
+    "4a#": 466.164,
+    "4b": 493.883,
+    "5c": 523.251,
+    "5c#": 554.365,
+    "5d": 587.330,
+    "5d#": 622.254,
+    "5e": 659.255,
+    "5f": 698.456,
+    "5f#": 739.989,
+    "5g": 783.991,
+    "5g#": 830.609,
+    "5a": 880,
+    "5a#": 932.328,
+    "5b": 987.767,
+    "6c": 1046.50,
+    "6c#": 1108.73,
+    "6d": 1174.66,
+    "6d#": 1244.51,
+    "6e": 1318.51,
+    "6f": 1396.91,
+    "6f#": 1479.98,
+    "6g": 1567.98,
+    "6g#": 1661.22,
+    "6a": 1760,
+    "6a#": 1864.66,
+    "6b": 1975.53,
+    "7c": 2093,
+    "7c#": 2217.46,
+}
+
 
 def _parse_note(note, duration=2, octave="6"):
     note = note.strip()
@@ -108,6 +112,7 @@ def _parse_note(note, duration=2, octave="6"):
     piano_note = note_octave + piano_note
     return piano_note, note_duration
 
+
 def _get_wave(tune, octave):
     """Returns the proper waveform to play the song along with the minimum
     frequency in the song.
@@ -121,7 +126,7 @@ def _get_wave(tune, octave):
     return sine.sine_wave(16000, min_freq), min_freq
 
 
-#pylint: disable-msg=too-many-arguments
+# pylint: disable-msg=too-many-arguments
 def _play_to_pin(tune, base_tone, min_freq, duration, octave, tempo):
     """Using the prepared input send the notes to the pin
     """
@@ -140,8 +145,7 @@ def _play_to_pin(tune, base_tone, min_freq, duration, octave, tempo):
                     sine_wave_sample = audiocore.RawSample(sine_wave)
                     base_tone.play(sine_wave_sample, loop=True)
                 else:
-                    base_tone.frequency = int(
-                        16000 * (PIANO[piano_note] / min_freq))
+                    base_tone.frequency = int(16000 * (PIANO[piano_note] / min_freq))
                     base_tone.play(loop=True)
 
         time.sleep(4 / note_duration * 60 / tempo)
@@ -151,7 +155,8 @@ def _play_to_pin(tune, base_tone, min_freq, duration, octave, tempo):
             base_tone.stop()
         time.sleep(0.02)
 
-#pylint: disable-msg=too-many-arguments
+
+# pylint: disable-msg=too-many-arguments
 def play(pin, rtttl, octave=None, duration=None, tempo=None):
     """Play notes to a digialio pin using ring tone text transfer language (rtttl).
     :param ~digitalio.DigitalInOut pin: the speaker pin
