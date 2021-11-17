0174764960ad189bccd6ea742862cea2635883c8
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-11-17 11:54:05 -0500

    Added audioio to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
-autodoc_mock_imports = ["pulseio", "pwmio"]
+autodoc_mock_imports = ["pulseio", "pwmio", "audioio"]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.4", None),
