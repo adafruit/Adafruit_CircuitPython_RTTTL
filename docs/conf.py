f87a6a7425de5393a8c0e6f2cdd4c2f2814e4fc0
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-11-17 11:48:40 -0500

    Add pwmio to mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
-autodoc_mock_imports = ["pulseio"]
+autodoc_mock_imports = ["pulseio", "pwmio"]
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.4", None),
